8409d5b8839bdc5f795be96eb7676c3a49b70926
Valera <portnovavalerija@mail.ru>
2022-07-20 13:57:58 +0300

    Add project files.

---
+++ FileAnalisys.API/Configuration/AutomapperApiBll.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FileAnalysis.API.Models;
+using FileAnalysis.BLL.Models;
+
+namespace FileAnalysis.API.Configuration
+{
+    // Mapping Request Models and Model from BLL
+    public class AutoMapperApiBll : Profile
+    {
+        public AutoMapperApiBll()
+        {
+            CreateMap<ScanModel, ScanResponse>();
+        }
+    }
+}
---
+++ FileAnalisys.BLL/Services/ProcessService.cs
@@ -0,0 +1,12 @@
+﻿using FileAnalysis.BLL.Models;
+
+namespace FileAnalysis.BLL.Services
+{
+    public class ProcessService : IProcessService
+    {
+        public ScanModel ScanFile(string url)
+        {
+            return null;
+        }
+    }
+}
