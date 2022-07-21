056c45ea632f60924bb062c61a550517021416c4
Valera <portnovavalerija@mail.ru>
2022-07-21 21:36:51 +0300

    added downloading chunks

--- FileAnalisys.BLL/Services/ScannerService.cs
+++ FileAnalisys.BLL/Services/ScannerService.cs
@@ -3,7 +3,6 @@ using FileAnalysis.BLL.Models;
 using RestSharp;
 using System.Net;
 using System.Security.Cryptography;
-using System.Text;
 
 namespace FileAnalysis.BLL.Services
 {
@@ -11,46 +10,58 @@ namespace FileAnalysis.BLL.Services
     {
         public ScanModel ScanFile(string url)
         {
-            var content = GetContent(url);
             var scanModel = new ScanModel();
+            var content = GetContent(url);
             scanModel.SHA1 = Hash(content); // hashing file
             scanModel.Result = SendRequest(content); // virus scanning 
             return scanModel;
         }
 
         // Parsing file
-        public string GetContent(string url)
+        public byte[] GetContent(string url)
         {
             var webRequest = WebRequest.Create(url);
             webRequest.Timeout = 36000;
 
             using (var response = webRequest.GetResponse())
             {
-                var fileSize = response.Headers.Get("Content-Length");
-                var fileSizeInMegaByte = Math.Round(Convert.ToDouble(fileSize) / 1024.0 / 1024.0, 2);
+                var fileSize = long.Parse(response.Headers.Get("Content-Length"));
+                var fileSizeInMegaByte = Math.Round(fileSize / 1024.0 / 1024.0, 2);
                 // Check if size is not very big
                 if (fileSizeInMegaByte > 200)
                     throw new FormatException("Size is too big");
-                using var content = response.GetResponseStream();
-                using var reader = new StreamReader(content);
-                return reader.ReadToEnd();
+
+                using (Stream content = response.GetResponseStream())
+                {
+                    byte[] buffer = new byte[32768]; //set the size of buffer (chunk)
+                    using (MemoryStream ms = new MemoryStream()) 
+                    {
+                        while (true) //loop to the end of the file
+                        {
+                            int read = content.ReadAsync(buffer, 0, buffer.Length).Result; //read each chunk
+                            if (read <= 0) //check for end of file
+                                return ms.ToArray();
+                            ms.Write(buffer, 0, read); 
+                        }
+                    }
+                }
             }
         }
 
         //Hashing file
-        public string Hash(string input)
+        public string Hash(byte[] input)
         {
             using var sha1 = SHA1.Create();
-            return Convert.ToHexString(sha1.ComputeHash(Encoding.UTF8.GetBytes(input)));
+            return Convert.ToHexString(sha1.ComputeHash(input));
         }
 
-
         // Sending request to scan virus
-        public string SendRequest(string content)
+        public string SendRequest(byte[] content)
         {
-            var client = new RestClient("ServiceUrl"); // TODO add real link
-            var request = new RestRequest("/resource/", Method.Post);
-            request.AddParameter("file", content);
+            var client = new RestClient("https://localhost:7030");
+            var request = new RestRequest("/api/process/", Method.Post);
+
+            request.AddQueryParameter("chunks", content.ToString());
             var response = client.Execute<string>(request);
 
             // Check response
@@ -62,6 +73,8 @@ namespace FileAnalysis.BLL.Services
 
             throw new ServiceUnavailableException(response.ErrorException.Message);
         }
+
+
 
     }
 }
