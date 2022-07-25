482e457a8590e1b7163398a05c6361a4b90c17ef
Valera <portnovavalerija@mail.ru>
2022-07-25 11:26:25 +0300

    added tests for ScannerController

---
+++ FileAnalisys.API.Tests/ScannerControllerTests.cs
@@ -0,0 +1,64 @@
+using AutoMapper;
+using FileAnalysis.API.Configuration;
+using FileAnalysis.API.Controllers;
+using FileAnalysis.BLL.Models;
+using FileAnalysis.BLL.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace FileAnalisys.API.Tests
+{
+    public class ScannerControllerTests
+    {
+        private Mock<IScannerService> _scannerService;
+        private ScannerController _sut;
+        private IMapper _autoMapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            _scannerService = new Mock<IScannerService>();
+            _autoMapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile<AutoMapperApiBll>()));
+            _sut = new ScannerController(_scannerService.Object, _autoMapper);
+        }
+
+        // Positive tests
+        [TestCase("http://example.com")]
+        [TestCase("http://www.example.com")]
+        [TestCase("https://example.com")]
+        [TestCase("https://www.example.com")]
+        public async Task ScanFile_ShouldScanFile(string url)
+        {
+            //given
+            _scannerService
+                .Setup(s => s.ScanFile(url))
+                .ReturnsAsync(new ScanModel());
+            
+            //when
+            await _sut.ScanFile(url);
+
+            //then
+            _scannerService.Verify(s => s.ScanFile(url), Times.Once());
+        }
+
+        //Negative tests with wrong URLs
+        [TestCase("www.example.com")]
+        [TestCase("not uri")]
+        [TestCase("https:/www.example.com")]
+        public async Task ScanFile_UriIsNotValid_ShouldThrowFormatException(string url)
+        {
+            //given
+            var expected = $"url: '{url}' is not valid.";
+
+            //when
+            var actual = Assert
+                .ThrowsAsync<FormatException>(async () => await _sut.ScanFile(url))!
+                .Message;
+
+            //then
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- FileAnalisys.BLL/Services/IScannerService.cs
+++ FileAnalisys.BLL/Services/IScannerService.cs
@@ -5,5 +5,9 @@ namespace FileAnalysis.BLL.Services
     public interface IScannerService
     {
         Task<ScanModel> ScanFile(string url);
+        Task<byte[]> GetContent(string url);
+        string Hash(byte[] input);
+        Task<string> SendRequest(byte[] content);
+        Task<string> CheckCacheBeforeSend(string hashKey, byte[] file);
     }
 }
